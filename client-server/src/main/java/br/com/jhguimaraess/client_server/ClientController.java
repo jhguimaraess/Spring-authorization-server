b9d4d04fc63699ba380570cef2780e0e81f48907
joao guimaraes <guimaraesjoao101@gmail.com>
2026-04-05 12:18:51 -0300

    feat: add webClient

--- client-server/src/main/java/br/com/jhguimaraess/client_server/ClientController.java
+++ client-server/src/main/java/br/com/jhguimaraess/client_server/ClientController.java
@@ -6,10 +6,19 @@ import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2Aut
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @RestController
 public class ClientController {
+
+    WebClient webClient;
+
+    public ClientController(WebClient.Builder builder){
+        this.webClient = builder
+                .baseUrl("http://127.0.0.1:9090")
+                .build();
+    }
 
     @GetMapping("/home")
     public Mono<String> home(
@@ -25,5 +34,15 @@ public class ClientController {
                 client.getRefreshToken().getTokenValue(),
                 oidcUser.getIdToken().getTokenValue(),
                 oidcUser.getClaims()));
+    }
+
+    @GetMapping("/tasks")
+    public Mono<String> getTasks(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient client){
+        return webClient
+                .get()
+                .uri("tasks")
+                .header("Authorization", "Bearer %s".formatted(client.getAccessToken().getTokenValue()))
+                .retrieve()
+                .bodyToMono(String.class);
     }
 }
